80581e08202d0a4c583bd56aa177115c033c9a03
Kdin <94251418+awesome13524@users.noreply.github.com>
2023-11-06 12:23:47 -0500

    Create table of books in library

--- library.js
+++ library.js
@@ -1,9 +1,13 @@
+const libraryDiv = document.querySelector('.library')
 const myLibrary = [];
 const input = document.querySelectorAll('input');
-const form = document.querySelector('form');
+const form = document.querySelector('#new-book');
+const table = document.querySelector('tbody')
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   addBookToLibrary();
+  addBookToTable();
+  libraryDiv.style.display = 'block';
 });
 
 function Book(title, author, pages, read) {
@@ -19,4 +23,25 @@ function addBookToLibrary() {
   let pages = input[2].value;
   let read = input[3].checked;
   myLibrary.push(new Book(title, author, pages, read))
+  for(let i = 0; i < 3; i++) {
+    input[i].value = ''
+  }
+  input[3].checked = false;
+}
+
+function addBookToTable() {
+  let book = myLibrary[myLibrary.length - 1];
+  const row = document.createElement('tr');
+  let cells = []  
+  for(i = 0; i < 4; i++) {
+    cells[i] = document.createElement('td');
+  }
+  cells[0].textContent = book.title;
+  cells[1].textContent = book.author;
+  cells[2].textContent = book.pages;
+  cells[3].textContent = book.read ? 'Yes' : 'No';
+  for(i = 0; i < 4; i++) {
+    row.appendChild(cells[i]);
+  }
+  table.appendChild(row);
 }
